a86d74e9ae2485ca0ffb922b83679fd7c45d581e
Nathan Kampbell <46498471+natekamp@users.noreply.github.com>
2025-04-09 18:14:19 -0600

    Add initial content

--- maxflora-site/src/App.js
+++ maxflora-site/src/App.js
@@ -1,23 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    document.title = "MaxFlorescence"
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Maxine Kampbell
       </header>
+      Under Construction!
+
+      <header className="App-header">
+        Projects
+      </header>
+      <ul>
+        <li>
+          2024 <a href="https://github.com/MaxFlorescence/spectral_redistricting">Congressional Redistricting Research</a>
+        </li>
+        <li>
+          2024 <a href="https://github.com/CS-CS415-Spring2024/t09">Company Management Web App</a> (private)
+        </li>
+        <li>
+          2024 <a href="https://github.com/CSU-CS214-Spring2014/assignment_autograding">GitHub Classroom Autograding Script</a> (private)
+        </li>
+        <li>
+          2023 <a href="https://github.com/Blanchard-lab/6D-Pose-Annotation-Tool">6D-Pose Interpolation Script</a> (private)
+        </li>
+        <li>
+          2022 <a href="https://github.com/MaxFlorescence/Blobby">3D Platformer Game</a>
+        </li>
+        <li>
+          2021 <a href="https://github.com/CSU-CS-314-Fall-2021/t23">Trip Planner Web App</a> (private)
+        </li>
+      </ul>
     </div>
   );
 }
